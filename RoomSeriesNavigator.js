7075fb24ca63208346c261a0b4e9f28a04d307ff
Jean-Christophe Bouillot <40596538+jcbouillot@users.noreply.github.com>
2023-02-21 16:53:09 +0100

    Update RoomSeriesNavigator.js

--- RoomSeriesNavigator.js
+++ RoomSeriesNavigator.js
@@ -22,14 +22,15 @@ async function updateUrl() {
   const anoise = await xapi.Status.RoomAnalytics.AmbientNoise.Level.A.get();
   const name = await xapi.Status.SystemUnit.BroadcastName.get();
   const airquality = await xapi.Status.Peripherals.ConnectedDevice.RoomAnalytics.AirQuality.Index.get();
-  const pccur = await xapi.Status.RoomAnalytics.PeopleCount.Current.get()
-  const pccap = await xapi.Status.RoomAnalytics.PeopleCount.Capacity.get()
-  const sound = await xapi.Status.RoomAnalytics.Sound.Level.A.get()
-  const ppresence = await xapi.Status.RoomAnalytics.PeoplePresence.get()
+  const pccur = await xapi.Status.RoomAnalytics.PeopleCount.Current.get();
+  const pccap = await xapi.Status.RoomAnalytics.PeopleCount.Capacity.get();
+  const sound = await xapi.Status.RoomAnalytics.Sound.Level.A.get();
+  const ppresence = await xapi.Status.RoomAnalytics.PeoplePresence.get();
 
   const url = baseUrl + '?temp=' + temp + '&humidity=' + hum + '&ambientnoise=' +anoise + '&name=' + name + '&airquality=' + airquality + '&peoplecount=' + pccur + '/' + pccap + '&sound=' + sound + '&ppresence=' + ppresence;
   xapi.Config.Standby.Signage.Url.set(url);
   console.log('signage url updated:', url);
 }
 
+//Update URL frequency 30sec
 setInterval(updateUrl, 100 * updateSec);
